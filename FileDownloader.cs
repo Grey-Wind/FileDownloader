26c7f4dce5dabf131309624cea81825377932124
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-10 12:22:34 +0800

    Update FileDownloader.cs

--- FileDownloader.cs
+++ FileDownloader.cs
@@ -1,73 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
-using System.Threading.Tasks;
 
-public class FileDownloader
+namespace FileDownloader
 {
-    private int _maxConcurrentDownloads;
-    private List<Task> _downloadTasks;
-
-    public FileDownloader()
+    public class Downloader
     {
-        _maxConcurrentDownloads = 4;
-        _downloadTasks = new List<Task>();
-    }
-
-    public async Task DownloadFile(string url, string saveDirectory, string fileName, int? maxConcurrentDownloads = null)
-    {
-        // 检查并发下载数是否合法
-        int concurrentDownloads = maxConcurrentDownloads ?? _maxConcurrentDownloads;
-        if (concurrentDownloads <= 0 || concurrentDownloads > 1024)
+        public void DownloadFiles(string[] urls, string[] savePaths)
         {
-            throw new ArgumentException("Concurrent downloads must be between 1 and 1024.");
-        }
-
-        // 创建Web请求
-        var webRequest = (HttpWebRequest)WebRequest.Create(url);
-        webRequest.Method = "HEAD";
-
-        // 获取文件大小
-        long fileSize;
-
-        using (var response = await webRequest.GetResponseAsync())
-        {
-            var contentLength = response.Headers.Get("Content-Length");
-            fileSize = Convert.ToInt64(contentLength);
-        }
-
-        // 计算每个线程需要下载的字节数
-        long chunkSize = fileSize / concurrentDownloads;
-
-        // 启动多个下载任务
-        for (int i = 0; i < concurrentDownloads; i++)
-        {
-            long startByte = i * chunkSize;
-            long endByte = (i == concurrentDownloads - 1) ? fileSize - 1 : (i + 1) * chunkSize - 1;
-
-            _downloadTasks.Add(DownloadChunk(url, saveDirectory, fileName, startByte, endByte));
-        }
-
-        // 等待所有下载任务完成
-        await Task.WhenAll(_downloadTasks);
-    }
-
-    private async Task DownloadChunk(string url, string saveDirectory, string fileName, long startByte, long endByte)
-    {
-        var webRequest = (HttpWebRequest)WebRequest.Create(url);
-        webRequest.AddRange(startByte, endByte);
-
-        using (var response = await webRequest.GetResponseAsync())
-        {
-            using (var contentStream = response.GetResponseStream())
+            if (urls.Length != savePaths.Length)
             {
-                string filePath = Path.Combine(saveDirectory, fileName);
+                throw new ArgumentException("URL 数量必须与保存路径数量相匹配。");
+            }
 
-                using (var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-                    await contentStream.CopyToAsync(fileStream);
-                }
+            for (int i = 0; i < urls.Length; i++)
+            {
+                DownloadFile(urls[i], savePaths[i]);
+            }
+        }
+
+        private void DownloadFile(string url, string savePath)
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(url, savePath);
+                Console.WriteLine($"文件 \"{savePath}\" 下载成功！");
             }
         }
     }
